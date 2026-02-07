43f2ad32495a56bc6ac30ef1d03a2228da64e94f
ime871356-hub <ime871356@gmail.com>
2026-02-07 14:10:16 +0530

    Update and rename script.js to rose.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let current = 1;
-
-function nextScreen() {
-  document.getElementById("screen" + current).classList.remove("active");
-  current++;
-  document.getElementById("screen" + current).classList.add("active");
-}
